8d69ad85491c3557e2b6fb516dee1d36c81ec4da
cwtablante <carlwilliam.tablante@gmail.com>
2021-10-28 08:15:06 +0800

    Screenshot browser

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/go-rod/rod"
+import (
+	"time"
+
+	"github.com/go-rod/rod"
+)
 
 func main() {
-	page := rod.New().MustConnect().MustPage("https://www.wikipedia.org/")
+	page := rod.New().MustConnect().MustPage("https://easyretro.io/publicboard/d0LnKN92OGdwxnFNhJt7Pow4T2b2/817d0a10-07ef-4408-bfb0-cd2e28c961c0")
+	time.Sleep(10 * time.Second)
 	page.MustWaitLoad().MustScreenshot("a.png")
 }
