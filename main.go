66bd68f56e03ef45816e779a133207d46523f1b6
cwtablante <carlwilliam.tablante@gmail.com>
2021-10-28 16:40:51 +0800

    Created Column struct

--- main.go
+++ main.go
@@ -6,30 +6,40 @@ import (
 	"github.com/go-rod/rod"
 )
 
+type Column struct {
+	Header   string
+	Contents []string
+}
+
+func NewColumns(driver *rod.Page) []Column {
+	var columns []Column
+	var contents []string
+	columnsElement := driver.MustElements(".easy-card-list")
+
+	for _, element := range columnsElement {
+		header := element.MustElement("h2.no-edit").MustText()
+
+		columnContents := element.MustElements("ul.column > li")
+		for _, contentElement := range columnContents {
+			text := contentElement.MustElement(".easy-board-front .easy-card-main-content .text").MustText()
+			contents = append(contents, text)
+		}
+
+		col := Column{
+			Header:   header,
+			Contents: contents,
+		}
+		columns = append(columns, col)
+	}
+
+	return columns
+}
+
 func main() {
-	// u := launcher.New().
-	// 	Delete("--headless").
-	// 	MustLaunch()
-
-	// page := rod.New().ControlURL(u).MustConnect().MustPage("https://easyretro.io/publicboard/d0LnKN92OGdwxnFNhJt7Pow4T2b2/817d0a10-07ef-4408-bfb0-cd2e28c961c0")
-	// page.MustWaitLoad().MustScreenshot("a.png")
-
 	page := rod.New().MustConnect().MustPage("https://easyretro.io/publicboard/d0LnKN92OGdwxnFNhJt7Pow4T2b2/817d0a10-07ef-4408-bfb0-cd2e28c961c0")
 	page.MustWaitElementsMoreThan(".easy-card-list", 0)
 
-	columns := page.MustWaitLoad().MustElements(".easy-card-list")
+	columns := NewColumns(page)
 
-	for _, column := range columns {
-		header := column.MustElement("h2.no-edit").MustText()
-
-		fmt.Println(header)
-
-		contents := column.MustElements("ul.column > li")
-
-		// Listing out the column's contents
-		for _, content := range contents {
-			text := content.MustElement(".easy-board-front .easy-card-main-content .text").MustText()
-			fmt.Println(text)
-		}
-	}
+	fmt.Println(columns)
 }
