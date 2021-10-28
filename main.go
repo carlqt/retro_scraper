e15073eb0fc72c2b951e18f1d21d1b3b441105fb
cwtablante <carlwilliam.tablante@gmail.com>
2021-10-28 11:00:50 +0800

    Fetch headers text

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
-	"time"
+	"fmt"
 
 	"github.com/go-rod/rod"
 )
 
 func main() {
+	// u := launcher.New().
+	// 	Delete("--headless").
+	// 	MustLaunch()
+
+	// page := rod.New().ControlURL(u).MustConnect().MustPage("https://easyretro.io/publicboard/d0LnKN92OGdwxnFNhJt7Pow4T2b2/817d0a10-07ef-4408-bfb0-cd2e28c961c0")
+	// page.MustWaitLoad().MustScreenshot("a.png")
+
 	page := rod.New().MustConnect().MustPage("https://easyretro.io/publicboard/d0LnKN92OGdwxnFNhJt7Pow4T2b2/817d0a10-07ef-4408-bfb0-cd2e28c961c0")
-	time.Sleep(10 * time.Second)
-	page.MustWaitLoad().MustScreenshot("a.png")
+	page.MustWaitElementsMoreThan(".column-header", 1)
+
+	headers := page.MustWaitLoad().MustElements(".column-header")
+
+	for _, header := range headers {
+		value := header.MustElement("h2.no-edit").MustText()
+
+		fmt.Println(value)
+	}
 }
