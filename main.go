6f7abdb04f236822b89bd6875dad69ffdb42af46
cwtablante <carlwilliam.tablante@gmail.com>
2021-10-30 07:48:10 +0800

    Write to CSV

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"encoding/csv"
+	"os"
 
 	"github.com/go-rod/rod"
 )
@@ -13,10 +14,10 @@ type Column struct {
 
 func NewColumns(driver *rod.Page) []Column {
 	var columns []Column
-	var contents []string
 	columnsElement := driver.MustElements(".easy-card-list")
 
 	for _, element := range columnsElement {
+		contents := make([]string, 1)
 		header := element.MustElement("h2.no-edit").MustText()
 
 		columnContents := element.MustElements("ul.column > li")
@@ -35,11 +36,62 @@ func NewColumns(driver *rod.Page) []Column {
 	return columns
 }
 
+func exportToCSV(columns []Column) {
+	// convert columns into something
+	inputs := convertToCsvInput(columns)
+
+	// maybe add timestamp
+	file, err := os.Create("easyretro.csv")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, input := range inputs {
+		_ = writer.Write(input)
+	}
+}
+
+func convertToCsvInput(columns []Column) [][]string {
+	// detect dimensions
+	// detect number of max rows
+	var rowsCount int
+	for _, column := range columns {
+		if rowsCount < len(column.Contents) {
+			rowsCount = len(column.Contents)
+		}
+	}
+
+	// detect number of columns
+	columnsCount := len(columns)
+
+	// initialize the board
+	cells := make([][]string, rowsCount+1)
+	for i := range cells {
+		cells[i] = make([]string, columnsCount)
+	}
+
+	for y, column := range columns {
+		cells[0][y] = column.Header
+
+		for x, content := range column.Contents {
+			cells[x+1][y] = content
+		}
+	}
+
+	return cells
+}
+
 func main() {
 	page := rod.New().MustConnect().MustPage("https://easyretro.io/publicboard/d0LnKN92OGdwxnFNhJt7Pow4T2b2/817d0a10-07ef-4408-bfb0-cd2e28c961c0")
 	page.MustWaitElementsMoreThan(".easy-card-list", 0)
 
 	columns := NewColumns(page)
 
-	fmt.Println(columns)
+	exportToCSV(columns)
 }
